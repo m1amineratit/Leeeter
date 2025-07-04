c4390e03aaffa7ffa039e298da877e5cb008a030
Hcode7 <ratitamine3@gmail.com>
2025-07-04 21:29:27 +0100

    .

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import Business, Contact, Location, Hour, Social, Media, FAQ, Card
 
 # Create your tests here.
+
+
