ec0550cb47730477957b999ecb882aa496519f49
Hcode7 <ratitamine3@gmail.com>
2025-07-23 01:25:38 +0100

    test apis and solve problems

--- leeeter/settings.py
+++ leeeter/settings.py
@@ -145,5 +145,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True 
 EMAIL_HOST_USER = 'amineratit6@gmail.com' 
-EMAIL_HOST_PASSWORD = 'mhni kjww malr uhwo'
+EMAIL_HOST_PASSWORD =os.environ.get('EMAIL_HOST_PASSWORD')
 
